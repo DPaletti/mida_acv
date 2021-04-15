023abb00e3cfbf0747deca49134c386e49e3a614
dpaletti <danielepaletti98@gmail.com>
2021-04-15 16:26:57 +0200

    Plots and directory refactoring

--- src/mida_acv/main.py
+++ src/mida_acv/main.py
@@ -1,22 +1,28 @@
+from pathlib import Path
 from typing import List, Dict
 
 from mida_acv.parsing import parse
 from mida_acv.preprocessing import remove_incremental_column, simplify_path
 from mida_acv.utilities import write_to_csv
+from mida_acv.visualization import visualize_path, visualize_signal, visualize_all
 import pandas as pd
 
 
 def main():
     data_path = "./data"
+    simplified_data_path = "./data/simplified_datasets"
     resource_path = "./resources/simplified_datasets"
-    data_dict: Dict[str, List[pd.Dataframe]] = remove_incremental_column(
-        parse(data_path)
-    )
-    data_dict_simplified = simplify_path(data_dict, 0.000001)
-    write_to_csv(resource_path, data_dict_simplified)
-    # visualize_path(
-    #    data_dict_simplified["stem-double"][0], data_path, data_dict["stem-double"][0]
+    # data_dict: Dict[str, List[pd.Dataframe]] = remove_incremental_column(
+    #    parse(data_path)
     # )
-
-
-main()
+    # data_dict_simplified = simplify_path(data_dict, 0.000001)
+    # write_to_csv(resource_path, data_dict_simplified)
+    visualize_all(
+        "./resources/plots",
+        "leoni",
+        35,
+        pd.read_csv(
+            data_path + "/stem/single_driver/Calibrated_Jessica_Leoni_35_stem.csv"
+        ),
+        pd.read_csv(simplified_data_path + "/stem/single/timeseries-12.csv"),
+    )
--- src/mida_acv/visualization.py
+++ src/mida_acv/visualization.py
@@ -1,25 +1,26 @@
 from pathlib import Path
+from typing import Tuple, List
 
 import pandas as pd
 import plotly.graph_objects as go
 
 
-def visualize_path(df: pd.DataFrame, data_path: str, df_full: pd.DataFrame = None):
+def visualize_path(df: pd.DataFrame, mapbox_token_path: str, *args):
     fig = go.Figure(
         go.Scattermapbox(
-            mode="markers+lines",
+            mode="lines",
             lon=df["Longitude"],
             lat=df["Latitude"],
-            marker={"size": 10},
+            marker={"size": 5},
         )
     )
-    if df_full is not None:
+    for _df in args:
         fig.add_trace(
             go.Scattermapbox(
-                mode="markers+lines",
-                lon=df_full["Longitude"],
-                lat=df_full["Latitude"],
-                marker={"size": 3},
+                mode="lines",
+                lon=_df["Longitude"],
+                lat=_df["Latitude"],
+                marker={"size": 5},
             )
         )
 
@@ -27,7 +28,134 @@ def visualize_path(df: pd.DataFrame, data_path: str, df_full: pd.DataFrame = Non
         margin={"l": 0, "t": 0, "b": 0, "r": 0},
         mapbox={
             "style": "stamen-terrain",
-            "accesstoken": Path(data_path).joinpath(".mapbox_token").read_text(),
+            "accesstoken": Path(mapbox_token_path)
+            .joinpath(".mapbox_token")
+            .read_text(),
         },
     )
     fig.show()
+
+
+def visualize_signal(
+    df: pd.DataFrame, signal: str, unit: str, label, *args: Tuple[pd.DataFrame, str]
+):
+    fig = go.Figure(
+        go.Scatter(
+            mode="markers+lines",
+            x=df["Timestamp"],
+            y=df[signal if signal == "Speed" else signal + "x"],
+            marker={"size": 3},
+            name=label,
+        )
+    )
+
+    fig.update_layout(
+        xaxis={"title": "Time (s)"},
+        yaxis={"title": signal.replace("_", "") + " (" + unit + ")"},
+        width=1920,
+        height=1080,
+        font=dict(size=18),
+        template="plotly_white",
+    )
+
+    if signal == "Speed":
+        for _df, _label in args:
+            fig.add_trace(
+                go.Scatter(
+                    mode="markers+lines",
+                    x=_df["Timestamp"],
+                    y=_df["Speed"],
+                    marker={"size": 3},
+                    name=_label,
+                )
+            )
+    else:
+        dfs: List[pd.DataFrame] = []
+        labels: List[str] = []
+        for t in args:
+            dfs.append(t[0])
+            labels.append(t[1])
+        else:
+            for _df, _label, axis in zip(dfs, labels, ["y", "z"]):
+                fig.add_trace(
+                    go.Scatter(
+                        mode="markers+lines",
+                        x=_df["Timestamp"],
+                        y=_df["Speed" if signal == "Speed" else signal + axis],
+                        marker={"size": 3},
+                        name=_label,
+                    )
+                )
+    return fig
+
+
+def visualize_all(
+    plot_path: str,
+    driver: str,
+    weight: int,
+    df: pd.DataFrame,
+    simplified_df: pd.DataFrame,
+):
+    for to_visualize, unit, full_name in zip(
+        ["Speed", "A", "G", "Jerk_"],
+        ["km/h", "m/s^2", "grad", "m/s^3"],
+        ["Speed", "Acceleration", "Orientation", "Jerk"],
+    ):
+        if to_visualize == "Speed":
+            visualize_signal(
+                df, "Speed", unit, full_name + " on the full path"
+            ).write_image(
+                str(
+                    Path(plot_path).joinpath(
+                        full_name.lower()
+                        + "_full_"
+                        + driver
+                        + "_"
+                        + str(weight)
+                        + ".png"
+                    )
+                ),
+            )
+            visualize_signal(
+                df,
+                "Speed",
+                "km/h",
+                "Speed on the full path",
+                (simplified_df, "Speed on the simplified path"),
+            ).write_image(
+                str(
+                    Path(plot_path).joinpath(
+                        "speed_full_simplified_" + driver + str(weight) + ".png"
+                    )
+                )
+            )
+        else:
+            visualize_signal(
+                df,
+                to_visualize,
+                unit,
+                "Pitch" if full_name == "Orientation" else full_name + " along x axis",
+                (
+                    df,
+                    "Yaw"
+                    if full_name == "Orientation"
+                    else full_name + " along y axis",
+                ),
+                (
+                    df,
+                    "Roll"
+                    if full_name == "Orientation"
+                    else full_name + " along z axis",
+                ),
+            ).write_image(
+                str(
+                    Path(plot_path).joinpath(
+                        full_name.lower()
+                        + "_full_"
+                        + driver
+                        + "_"
+                        + str(weight)
+                        + ".png"
+                    )
+                )
+            )
